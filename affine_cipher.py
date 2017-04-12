dc00cad046c62ee548cb9f658409b227b3a6c61a
chrisgarland <1chris.garland1@gmail.com>
2017-04-12 20:13:34 +0800

    upgraded to caesar cipher

--- affine_cipher.py
+++ affine_cipher.py
@@ -1,21 +1,37 @@
+# .../Ass1/AffineCipher/affine_cipher.py
 import sys
 
+shift = 27
 
 def main(in_file, mode):
     if mode is 'e':
-        with open(in_file, 'r+') as f:
-            encrypt(f)
+        encrypt(in_file)
     elif mode is 'd':
-        with open(in_file, 'r+') as f:
-            decrypt(f)
+        decrypt(in_file)
 
 
 def encrypt(in_file):
-    pass
+    file_contents = in_file.readlines()
+    f.seek(0)
+    for line in file_contents:
+        for c in line:
+            if c.isalpha():
+                ch = chr(ord(c)+(shift%26))
+            else:
+                ch =  c
+            in_file.write(ch)
 
 
 def decrypt(in_file):
-    pass
+    file_contents = in_file.readlines()
+    f.seek(0)
+    for line in file_contents:
+        for c in line:
+            if c.isalpha():
+                ch = chr(ord(c)-(shift%26))
+            else:
+                ch =  c
+            in_file.write(ch)
 
 
 def corr_num_args(argv):
@@ -23,7 +39,9 @@ def corr_num_args(argv):
 
 if __name__ == '__main__':
     if corr_num_args(sys.argv):
-        main(sys.argv[0], sys.argv[2])
+        f = open(sys.argv[1], 'r+')
+        main(f, sys.argv[2])
+        f.close()
     else:
         print 'Wrong number of args, must enter existing filename followed by \'e\' or \'d\''
         sys.exit(1)
