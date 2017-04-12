c5aadb67d8fbfb0be5da3414fc354f9b461712b2
chrisgarland <1chris.garland1@gmail.com>
2017-04-12 16:31:03 +0800

    main complete

--- affine_cipher.py
+++ affine_cipher.py
@@ -1,1 +1,30 @@
-print 'Hello World!'
+import sys
+
+
+def main(in_file, mode):
+    if mode is 'e':
+        with open(in_file, 'r+') as f:
+            encrypt(f)
+    elif mode is 'd':
+        with open(in_file, 'r+') as f:
+            decrypt(f)
+
+
+def encrypt(in_file):
+    pass
+
+
+def decrypt(in_file):
+    pass
+
+
+def corr_num_args(argv):
+    return len(argv) is 3
+
+if __name__ == '__main__':
+    if corr_num_args(sys.argv):
+        main(sys.argv[0], sys.argv[2])
+    else:
+        print 'Wrong number of args, must enter existing filename followed by \'e\' or \'d\''
+        sys.exit(1)
+
