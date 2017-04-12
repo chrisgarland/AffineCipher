624598b6a904df8ded798c3ff310c0b71e832055
chrisgarland <1chris.garland1@gmail.com>
2017-04-12 21:31:51 +0800

    improved file handeling

--- affine_cipher.py
+++ affine_cipher.py
@@ -1,13 +1,20 @@
 # .../Ass1/AffineCipher/affine_cipher.py
 import sys
+import os.path
 
 shift = 27
+
 
 def main(in_file, mode):
     if mode is 'e':
         encrypt(in_file)
     elif mode is 'd':
-        decrypt(in_file)
+        tokens = in_file.name.split('.')
+        if tokens[2] != 'encr':
+            print "Can only decrypt file with extension: '.encr'"
+            sys.exit(1)
+        else:
+            decrypt(in_file)
 
 
 def encrypt(in_file):
@@ -16,10 +23,12 @@ def encrypt(in_file):
     for line in file_contents:
         for c in line:
             if c.isalpha():
-                ch = chr(ord(c)+(shift%26))
+                ch = chr(ord(c)+(shift % 26))
             else:
-                ch =  c
+                ch = c
             in_file.write(ch)
+    print in_file.name + ' encrypted'
+    os.rename(in_file.name, in_file.name + '.encr')
 
 
 def decrypt(in_file):
@@ -28,21 +37,25 @@ def decrypt(in_file):
     for line in file_contents:
         for c in line:
             if c.isalpha():
-                ch = chr(ord(c)-(shift%26))
+                ch = chr(ord(c)-(shift % 26))
             else:
-                ch =  c
+                ch = c
             in_file.write(ch)
 
 
 def corr_num_args(argv):
     return len(argv) is 3
 
+
+def corr_order_args(argv):
+    return os.path.isfile(argv[1]) and (argv[2] is 'e' or argv[2] is 'd')
+
+
 if __name__ == '__main__':
-    if corr_num_args(sys.argv):
+    if corr_num_args(sys.argv) and corr_order_args(sys.argv):
         f = open(sys.argv[1], 'r+')
         main(f, sys.argv[2])
         f.close()
     else:
-        print 'Wrong number of args, must enter existing filename followed by \'e\' or \'d\''
+        print 'Invalid args: enter existing filename followed by \'e\' or \'d\''
         sys.exit(1)
-
